d88e00b0dcd835e040bef5c6c6282f3545c58708
Devendra Suryavanshi <devendrasooryavanshee@gmail.com>
2025-06-03 12:36:01 +0530

    update: url

--- frontend/src/data/urls.ts
+++ frontend/src/data/urls.ts
@@ -1,2 +1,2 @@
 export const CLIENT_URL = 'https://e-charge-hub.vercel.app/';
-export const API_URL = 'https://e-charge-hub.vercel.app/api'
+export const API_URL = 'https://e-charge-hub.onrender.com/api'
