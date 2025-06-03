5740aaca78fb1ebd91bb9b67ab3a187107b89ed4
Devendra Suryavanshi <devendrasooryavanshee@gmail.com>
2025-06-03 12:28:27 +0530

    update: url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ const MONGODB_URI = process.env.MONGODB_URI || '';
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 
